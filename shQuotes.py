8b76f46ad0bc80a86f14b0c99a33eae202b5e1d2
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2021-08-24 15:36:17 -0700

    reading quotes table using google sheets api

---
+++ shQuotes.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+quotesSheet = '1oOaSeWJbYQHUe8rPuvq3JMFZNjTrDId-13rWAWoxRh4'
+qsRange = 'A1:A72'
+
+def main():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', scopes)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', scopes)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=quotesSheet,
+                                range=qsRange).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+            print(row[0])
+
+if __name__ == '__main__':
+    main()
