e6ea10641d1369133b5b826e7cbd7752eedb6be5
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2021-08-31 13:39:18 -0700

    all the parts of the bot for final jarva bot

---
+++ jarvaAll.py
@@ -0,0 +1,215 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from playsound import playsound
+from google.cloud import texttospeech
+import random
+import face_recognition
+import cv2
+import numpy as np
+from playsound import playsound
+from google.cloud import texttospeech
+from time import gmtime, strftime
+
+scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+nums = [f"A{i+1}" for i in range(72)]
+ranQuoteNum = random.choice(nums)
+quotesSheet = '1oOaSeWJbYQHUe8rPuvq3JMFZNjTrDId-13rWAWoxRh4'
+qsRange = ranQuoteNum
+
+
+# ser = serial.Serial('/dev/ttyACM0')
+video_capture = cv2.VideoCapture(0)
+
+# print(cv2.getBuildInformation())
+
+dani0 = face_recognition.load_image_file("dani0.jpg")
+dani0_face_encoding = face_recognition.face_encodings(dani0)[0]
+
+dani1 = face_recognition.load_image_file("dani1.jpg")
+dani1_face_encoding = face_recognition.face_encodings(dani1)[0]
+
+dani2 = face_recognition.load_image_file("dani2.jpg")
+dani2_face_encoding = face_recognition.face_encodings(dani2)[0]
+
+known_face_encodings = [
+    dani0_face_encoding,
+    dani1_face_encoding,
+    dani2_face_encoding
+]
+known_face_names = [
+    "dani0",
+    "dani1",
+    "dani2"
+]
+
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+i=0
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Only process every other frame of video to save time
+    i=i+1
+    if i%100==0:
+        i=0
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+
+            now = strftime("%H:%M:%S", gmtime())
+            print(now)
+
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+                client = texttospeech.TextToSpeechClient()
+
+                # Set the text input to be synthesized
+                synthesis_input = texttospeech.SynthesisInput(text="Hello, Dani")
+
+                # Build the voice request, select the language code ("en-US") and the ssml
+                # voice gender ("neutral")
+                voice = texttospeech.VoiceSelectionParams(
+                    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+                )
+
+                # Select the type of audio file you want returned
+                audio_config = texttospeech.AudioConfig(
+                    audio_encoding=texttospeech.AudioEncoding.MP3
+                )
+
+                # Perform the text-to-speech request on the text input with the selected
+                # voice parameters and audio file type
+                response = client.synthesize_speech(
+                    input=synthesis_input, voice=voice, audio_config=audio_config
+                )
+
+                # The response's audio_content is binary.
+                with open("output.mp3", "wb") as out:
+                    # Write the response to the output file.
+                    out.write(response.audio_content)
+                    print('Audio content written to file "output.mp3"')
+                    playsound('/home/dani/Desktop/jar/output.mp3')
+
+            face_names.append(name)
+
+
+    process_this_frame = not process_this_frame
+
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+def main():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', scopes)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', scopes)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=quotesSheet,
+                                range=qsRange).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+            # Instantiates a client
+            client = texttospeech.TextToSpeechClient()
+
+            # Set the text input to be synthesized
+            synthesis_input = texttospeech.SynthesisInput(text=row[0])
+
+            # Build the voice request, select the language code ("en-US") and the ssml
+            # voice gender ("neutral")
+            voice = texttospeech.VoiceSelectionParams(
+                language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+            )
+
+            # Select the type of audio file you want returned
+            audio_config = texttospeech.AudioConfig(
+                audio_encoding=texttospeech.AudioEncoding.MP3
+            )
+
+            # Perform the text-to-speech request on the text input with the selected
+            # voice parameters and audio file type
+            response = client.synthesize_speech(
+                input=synthesis_input, voice=voice, audio_config=audio_config
+            )
+
+            # The response's audio_content is binary.
+            with open("output.mp3", "wb") as out:
+                # Write the response to the output file.
+                out.write(response.audio_content)
+                print('Audio content written to file "output.mp3"')
+                playsound('/home/dani/Desktop/jar/output.mp3')
+
+if __name__ == '__main__':
+    main()
