3bfbb7356c87e5da875f8ad2a71924f698ffba19
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2021-09-05 20:09:16 -0700

    new quotes from list

--- shQuotes.py
+++ shQuotes.py
@@ -1,82 +1,36 @@
-from __future__ import print_function
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+import random
 from playsound import playsound
 from google.cloud import texttospeech
-import random
+import quotes
 
-scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+ranQuote = random.choice(quotes.quotesList)
 
-nums = [f"A{i+1}" for i in range(72)]
-ranQuoteNum = random.choice(nums)
-quotesSheet = '1oOaSeWJbYQHUe8rPuvq3JMFZNjTrDId-13rWAWoxRh4'
-qsRange = ranQuoteNum
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
 
+# Set the text input to be synthesized
+synthesis_input = texttospeech.SynthesisInput(text=ranQuote)
 
+# Build the voice request, select the language code ("en-US") and the ssml
+# voice gender ("neutral")
+voice = texttospeech.VoiceSelectionParams(
+    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+)
 
-def main():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', scopes)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', scopes)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+# Select the type of audio file you want returned
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
 
-    service = build('sheets', 'v4', credentials=creds)
+# Perform the text-to-speech request on the text input with the selected
+# voice parameters and audio file type
+response = client.synthesize_speech(
+    input=synthesis_input, voice=voice, audio_config=audio_config
+)
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=quotesSheet,
-                                range=qsRange).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        for row in values:
-            # Instantiates a client
-            client = texttospeech.TextToSpeechClient()
-
-            # Set the text input to be synthesized
-            synthesis_input = texttospeech.SynthesisInput(text=row[0])
-
-            # Build the voice request, select the language code ("en-US") and the ssml
-            # voice gender ("neutral")
-            voice = texttospeech.VoiceSelectionParams(
-                language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-            )
-
-            # Select the type of audio file you want returned
-            audio_config = texttospeech.AudioConfig(
-                audio_encoding=texttospeech.AudioEncoding.MP3
-            )
-
-            # Perform the text-to-speech request on the text input with the selected
-            # voice parameters and audio file type
-            response = client.synthesize_speech(
-                input=synthesis_input, voice=voice, audio_config=audio_config
-            )
-
-            # The response's audio_content is binary.
-            with open("output.mp3", "wb") as out:
-                # Write the response to the output file.
-                out.write(response.audio_content)
-                print('Audio content written to file "output.mp3"')
-                playsound('/home/dani/Desktop/jar/output.mp3')
-
-if __name__ == '__main__':
-    main()
+# The response's audio_content is binary.
+with open("output.mp3", "wb") as out:
+    # Write the response to the output file.
+    out.write(response.audio_content)
+    print('Audio content written to file "output.mp3"')
+    playsound('/home/dani/Desktop/jar/output.mp3')
