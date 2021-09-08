ebb6884d688694c339266ca079fe7cec85e924d6
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2021-09-07 21:27:23 -0700

    added live listening for voice search

--- jarvaAll.py
+++ jarvaAll.py
@@ -1,26 +1,25 @@
-from __future__ import print_function
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from __future__ import print_function, division
+import random
 from playsound import playsound
 from google.cloud import texttospeech
-import random
+import quotes
 import face_recognition
 import cv2
 import numpy as np
 from playsound import playsound
 from google.cloud import texttospeech
 from time import gmtime, strftime
+from selenium import webdriver
+import re
+import sys
+from google.cloud import speech
+import pyaudio
+from six.moves import queue
+import webbrowser
 
-scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-nums = [f"A{i+1}" for i in range(72)]
-ranQuoteNum = random.choice(nums)
-quotesSheet = '1oOaSeWJbYQHUe8rPuvq3JMFZNjTrDId-13rWAWoxRh4'
-qsRange = ranQuoteNum
 
+ranQuote = random.choice(quotes.quotesList)
 
 # ser = serial.Serial('/dev/ttyACM0')
 video_capture = cv2.VideoCapture(0)
@@ -93,6 +92,10 @@ while True:
                 # Set the text input to be synthesized
                 synthesis_input = texttospeech.SynthesisInput(text="Hello, Dani")
 
+                # Release handle to the webcam
+                video_capture.release()
+                cv2.destroyAllWindows()
+
                 # Build the voice request, select the language code ("en-US") and the ssml
                 # voice gender ("neutral")
                 voice = texttospeech.VoiceSelectionParams(
@@ -110,12 +113,215 @@ while True:
                     input=synthesis_input, voice=voice, audio_config=audio_config
                 )
 
+                with open("output.mp3", "wb") as out:
+                    # Write the response to the output file.
+                    out.write(response.audio_content)
+                    print('Audio content written to file "output.mp3"')
+                    playsound('/home/dani/Desktop/jar/output.mp3')
+
+                # Instantiates a client
+                client = texttospeech.TextToSpeechClient()
+
+                # Set the text input to be synthesized
+                synthesis_input = texttospeech.SynthesisInput(text=ranQuote)
+
+                # Build the voice request, select the language code ("en-US") and the ssml
+                # voice gender ("neutral")
+                voice = texttospeech.VoiceSelectionParams(
+                    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+                )
+
+                # Select the type of audio file you want returned
+                audio_config = texttospeech.AudioConfig(
+                    audio_encoding=texttospeech.AudioEncoding.MP3
+                )
+
+                # Perform the text-to-speech request on the text input with the selected
+                # voice parameters and audio file type
+                response = client.synthesize_speech(
+                    input=synthesis_input, voice=voice, audio_config=audio_config
+                )
                 # The response's audio_content is binary.
                 with open("output.mp3", "wb") as out:
                     # Write the response to the output file.
                     out.write(response.audio_content)
                     print('Audio content written to file "output.mp3"')
                     playsound('/home/dani/Desktop/jar/output.mp3')
+
+
+
+                url = 'google.com'
+
+                # Audio recording parameters
+                RATE = 16000
+                CHUNK = int(RATE / 10)  # 100ms
+
+
+                class MicrophoneStream(object):
+                    """Opens a recording stream as a generator yielding the audio chunks."""
+
+                    def __init__(self, rate, chunk):
+                        self._rate = rate
+                        self._chunk = chunk
+
+                        # Create a thread-safe buffer of audio data
+                        self._buff = queue.Queue()
+                        self.closed = True
+
+                    def __enter__(self):
+                        self._audio_interface = pyaudio.PyAudio()
+                        self._audio_stream = self._audio_interface.open(
+                            format=pyaudio.paInt16,
+                            # The API currently only supports 1-channel (mono) audio
+                            # https://goo.gl/z757pE
+                            channels=1,
+                            rate=self._rate,
+                            input=True,
+                            frames_per_buffer=self._chunk,
+                            # Run the audio stream asynchronously to fill the buffer object.
+                            # This is necessary so that the input device's buffer doesn't
+                            # overflow while the calling thread makes network requests, etc.
+                            stream_callback=self._fill_buffer,
+                        )
+
+                        self.closed = False
+
+                        return self
+
+                    def __exit__(self, type, value, traceback):
+                        self._audio_stream.stop_stream()
+                        self._audio_stream.close()
+                        self.closed = True
+                        # Signal the generator to terminate so that the client's
+                        # streaming_recognize method will not block the process termination.
+                        self._buff.put(None)
+                        self._audio_interface.terminate()
+
+                    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+                        """Continuously collect data from the audio stream, into the buffer."""
+                        self._buff.put(in_data)
+                        return None, pyaudio.paContinue
+
+                    def generator(self):
+                        while not self.closed:
+                            # Use a blocking get() to ensure there's at least one chunk of
+                            # data, and stop iteration if the chunk is None, indicating the
+                            # end of the audio stream.
+                            chunk = self._buff.get()
+                            if chunk is None:
+                                return
+                            data = [chunk]
+
+                            # Now consume whatever other data's still buffered.
+                            while True:
+                                try:
+                                    chunk = self._buff.get(block=False)
+                                    if chunk is None:
+                                        return
+                                    data.append(chunk)
+                                except queue.Empty:
+                                    break
+
+                            yield b"".join(data)
+
+
+                def listen_print_loop(responses):
+                    """Iterates through server responses and prints them.
+
+                    The responses passed is a generator that will block until a response
+                    is provided by the server.
+
+                    Each response may contain multiple results, and each result may contain
+                    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+                    print only the transcription for the top alternative of the top result.
+
+                    In this case, responses are provided for interim results as well. If the
+                    response is an interim one, print a line feed at the end of it, to allow
+                    the next result to overwrite it, until the response is a final one. For the
+                    final one, print a newline to preserve the finalized transcription.
+                    """
+                    num_chars_printed = 0
+                    for response in responses:
+                        if not response.results:
+                            continue
+
+                        # The `results` list is consecutive. For streaming, we only care about
+                        # the first result being considered, since once it's `is_final`, it
+                        # moves on to considering the next utterance.
+                        result = response.results[0]
+                        if not result.alternatives:
+                            continue
+
+                        # Display the transcription of the top alternative.
+                        transcript = result.alternatives[0].transcript
+
+                        # Display interim results, but with a carriage return at the end of the
+                        # line, so subsequent lines will overwrite them.
+                        #
+                        # If the previous result was longer than this one, we need to print
+                        # some extra spaces to overwrite the previous result
+                        overwrite_chars = " " * (num_chars_printed - len(transcript))
+
+                        if not result.is_final:
+                            sys.stdout.write(transcript + overwrite_chars + "\r")
+                            sys.stdout.flush()
+
+                            num_chars_printed = len(transcript)
+
+                        else:
+                            print_state = transcript + overwrite_chars
+                            print(print_state)
+
+                            if re.search(r"\b(please search)\b", transcript, re.I):
+                                print("searching..")
+                                search_string = transcript[14:]
+                                search_string = search_string.replace(' ', '+', )
+                                browser = webdriver.Chrome('chromedriver')
+                                for i in range(1):
+                                    matched_elements = browser.get("https://www.google.com/search?q=" +
+                                                                   search_string + "&start=" + str(i))
+
+                            if re.search(r"\b(exit|quit)\b", transcript, re.I):
+                                print("goodbye")
+                                break
+
+                            num_chars_printed = 0
+
+
+                def main():
+                    # See http://g.co/cloud/speech/docs/languages
+                    # for a list of supported languages.
+                    language_code = "en-US"  # a BCP-47 language tag
+
+                    client = speech.SpeechClient()
+                    config = speech.RecognitionConfig(
+                        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+                        sample_rate_hertz=RATE,
+                        language_code=language_code,
+                    )
+
+                    streaming_config = speech.StreamingRecognitionConfig(
+                        config=config, interim_results=True
+                    )
+
+                    with MicrophoneStream(RATE, CHUNK) as stream:
+                        audio_generator = stream.generator()
+                        requests = (
+                            speech.StreamingRecognizeRequest(audio_content=content)
+                            for content in audio_generator
+                        )
+
+                        responses = client.streaming_recognize(streaming_config, requests)
+
+                        # Now, put the transcription responses to use.
+                        listen_print_loop(responses)
+
+
+                if __name__ == "__main__":
+                    main()
+
+
+
 
             face_names.append(name)
 
@@ -146,70 +352,3 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
-
-def main():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', scopes)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', scopes)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    service = build('sheets', 'v4', credentials=creds)
-
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=quotesSheet,
-                                range=qsRange).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        for row in values:
-            # Instantiates a client
-            client = texttospeech.TextToSpeechClient()
-
-            # Set the text input to be synthesized
-            synthesis_input = texttospeech.SynthesisInput(text=row[0])
-
-            # Build the voice request, select the language code ("en-US") and the ssml
-            # voice gender ("neutral")
-            voice = texttospeech.VoiceSelectionParams(
-                language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-            )
-
-            # Select the type of audio file you want returned
-            audio_config = texttospeech.AudioConfig(
-                audio_encoding=texttospeech.AudioEncoding.MP3
-            )
-
-            # Perform the text-to-speech request on the text input with the selected
-            # voice parameters and audio file type
-            response = client.synthesize_speech(
-                input=synthesis_input, voice=voice, audio_config=audio_config
-            )
-
-            # The response's audio_content is binary.
-            with open("output.mp3", "wb") as out:
-                # Write the response to the output file.
-                out.write(response.audio_content)
-                print('Audio content written to file "output.mp3"')
-                playsound('/home/dani/Desktop/jar/output.mp3')
-
-if __name__ == '__main__':
-    main()
